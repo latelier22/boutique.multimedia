fbc2e2377d61caf1f614f08ea9b472bd4ee981eb
latelier22 <latelier.cyrille@gmail.com>
2025-05-16 14:52:18 +0200

    debut search image google

--- assets/admin/entry.js
+++ assets/admin/entry.js
@@ -7,3 +7,21 @@ import routes from './js/fos_js_routes.json';   // CHEMIN DANS assets/
 
 window.Routing = Routing;
 Routing.setRoutingData(routes);
+
+
+console.log('🟢 admin-perso entry.js chargé');
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.getElementById('search-images-btn');
+  if (!btn) {
+    console.warn('⚠️ Bouton search-images-btn non trouvé');
+    return;
+  }
+  btn.addEventListener('click', () => {
+    console.log('🟢 Bouton “Rechercher une image” cliqué');
+  });
+});
+
+
+
+import './controllers/image_search_controller';
