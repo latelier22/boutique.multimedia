bc819ce95941bb1ec83b75d1f95aa59ab553ac15
latelier22 <latelier.cyrille@gmail.com>
2025-05-18 14:16:46 +0200

    cropper js

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,8 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 // import '@symfony/autoimport';
 
+import '@symfony/ux-cropperjs';
+import 'cropperjs/dist/cropper.css'; 
+
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context('./controllers', true, /\.(j|t)sx?$/));
