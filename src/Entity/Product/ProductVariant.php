999d9c1e2638812b53bc3bc767295e7ed57b62f2
latelier22 <latelier.cyrille@gmail.com>
2025-05-31 03:38:19 +0000

    elastic search

--- src/Entity/Product/ProductVariant.php
+++ src/Entity/Product/ProductVariant.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Entity\Product;
 
+use BitBag\SyliusElasticsearchPlugin\Model\ProductVariantInterface as BitBagElasticsearchPluginVariant;
+use BitBag\SyliusElasticsearchPlugin\Model\ProductVariantTrait;
 use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\ProductVariant as BaseProductVariant;
+use Sylius\Component\Core\Model\ProductVariantInterface as BaseProductVariantInterface;
 use Sylius\Component\Product\Model\ProductVariantTranslationInterface;
 
 /**
@@ -14,10 +18,19 @@ use Sylius\Component\Product\Model\ProductVariantTranslationInterface;
  */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_product_variant')]
-class ProductVariant extends BaseProductVariant
+class ProductVariant extends BaseProductVariant implements BaseProductVariantInterface, BitBagElasticsearchPluginVariant
 {
+    use ProductVariantTrait;
+
     protected function createTranslation(): ProductVariantTranslationInterface
     {
         return new ProductVariantTranslation();
+    }
+
+    // 🔧 Correction : retour non-nullable requis par l'interface du plugin
+    public function getProduct(): ProductInterface
+    {
+        /** @var ProductInterface */
+        return parent::getProduct(); // ⚠️ Doit ne jamais être null !
     }
 }
