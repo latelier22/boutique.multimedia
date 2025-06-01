3e66a6a7cddbafdca881a6328d30f3ba6ca52fd3
latelier22 <latelier.cyrille@gmail.com>
2025-06-01 18:05:38 +0000

    MonsieurBiz\SyliusSearchPlugin autocomplete product ok reste taxon...

--- src/Entity/Product/ProductOption.php
+++ src/Entity/Product/ProductOption.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Entity\Product;
 
 use Doctrine\ORM\Mapping as ORM;
+use MonsieurBiz\SyliusSearchPlugin\Entity\Product\SearchableInterface;
+use MonsieurBiz\SyliusSearchPlugin\Model\Product\SearchableTrait;
 use Sylius\Component\Product\Model\ProductOption as BaseProductOption;
 use Sylius\Component\Product\Model\ProductOptionTranslationInterface;
 
@@ -14,10 +16,14 @@ use Sylius\Component\Product\Model\ProductOptionTranslationInterface;
  */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_product_option')]
-class ProductOption extends BaseProductOption
+class ProductOption extends BaseProductOption implements SearchableInterface
 {
+    use SearchableTrait;
+
     protected function createTranslation(): ProductOptionTranslationInterface
     {
         return new ProductOptionTranslation();
     }
 }
+
+
