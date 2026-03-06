cebac1616ed71e8db42661837f3ee20db9cdd917
latelier22 <latelier.cyrille@gmail.com>
2026-03-06 07:56:07 +0000

    upgrade sylius to 1.14 and sync schema

--- src/Repository/ProductVariantRepository.php
+++ src/Repository/ProductVariantRepository.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use Sylius\AdminOrderCreationPlugin\Doctrine\ORM\ProductVariantRepositoryInterface;
-use Sylius\AdminOrderCreationPlugin\Doctrine\ORM\ProductVariantRepositoryTrait;
 use Sylius\Bundle\CoreBundle\Doctrine\ORM\ProductVariantRepository as BaseProductVariantRepository;
 
-final class ProductVariantRepository extends BaseProductVariantRepository implements ProductVariantRepositoryInterface
+final class ProductVariantRepository extends BaseProductVariantRepository
 {
-    use ProductVariantRepositoryTrait;
 }
